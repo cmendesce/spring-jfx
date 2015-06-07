8d49d7af0264ccddb4364f507df8c7edb91e07ed
Carlos Mendes <cmendesce@gmail.com>
2015-06-07 17:16:07 -0300

    Added initial project

---
+++ src/main/java/com/github/cmendesce/App.java
@@ -0,0 +1,13 @@
+package com.github.cmendesce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
